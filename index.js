22052d59535fc428dad9061b7b0cea80eb97b2a0
Karthikeyan J <119804748+Karthikeyan-j1112@users.noreply.github.com>
2023-03-07 14:36:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require("node:http");
+
+const server = http.createServer((req,res)=>{
+    res.writeHead( 200, { "Content-Type": "text/html" } )
+    res.end( "<h1>Hello World!!</h1>" )
+})
+
+const port = process.env.PORT || 3000
+
+server.listen(port, ()=>console.log(`Server is running on port ${port}`))
